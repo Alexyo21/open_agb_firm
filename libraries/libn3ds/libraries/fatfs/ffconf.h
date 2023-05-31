a443dcf9f1b46e9b66efe08d001341bde4ea260e
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-05-31 23:01:35 +0200

    Update ffconf.h
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- libraries/libn3ds/libraries/fatfs/ffconf.h
+++ libraries/libn3ds/libraries/fatfs/ffconf.h
@@ -58,7 +58,7 @@
 
 #define FF_USE_STRFUNC	2
 #define FF_PRINT_LLI	1
-#define FF_PRINT_FLOAT	1
+#define FF_PRINT_FLOAT	0
 #define FF_STRF_ENCODE	3
 /* FF_USE_STRFUNC switches string functions, f_gets(), f_putc(), f_puts() and
 /  f_printf().
