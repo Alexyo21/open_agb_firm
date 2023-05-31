c29d2483378f89b93cf818ca21fe0219297bacc7
Fra <96795244+Alexyo21@users.noreply.github.com>
2023-05-31 23:02:10 +0200

    Update ffconf.h
    Signed-off-by: Fra <96795244+Alexyo21@users.noreply.github.com>

--- libraries/libn3ds/libraries/fatfs/ffconf.h
+++ libraries/libn3ds/libraries/fatfs/ffconf.h
@@ -42,7 +42,7 @@
 /* This option switches f_expand function. (0:Disable or 1:Enable) */
 
 
-#define FF_USE_CHMOD	0
+#define FF_USE_CHMOD	1
 /* This option switches attribute manipulation functions, f_chmod() and f_utime().
 /  (0:Disable or 1:Enable) Also FF_FS_READONLY needs to be 0 to enable this option. */
 
@@ -58,7 +58,7 @@
 
 #define FF_USE_STRFUNC	2
 #define FF_PRINT_LLI	1
-#define FF_PRINT_FLOAT	0
+#define FF_PRINT_FLOAT	2
 #define FF_STRF_ENCODE	3
 /* FF_USE_STRFUNC switches string functions, f_gets(), f_putc(), f_puts() and
 /  f_printf().
